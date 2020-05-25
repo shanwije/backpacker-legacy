35fbcdc0c0f33162230b8ec2f4135bc000d2c55c
Shan Wijenayaka <shanwijenayaka@outlook.com>
2020-05-25 20:51:57 +0530

    renamed project to backpacker

--- android/app/src/main/java/com/backpacker/MainActivity.java
+++ android/app/src/main/java/com/backpacker/MainActivity.java
@@ -1,4 +1,4 @@
-package com.rnboilerplate;
+package com.backpacker;
 
 import com.facebook.react.ReactActivity;
 
@@ -10,6 +10,6 @@ public class MainActivity extends ReactActivity {
    */
   @Override
   protected String getMainComponentName() {
-    return "RNBoilerPlate";
+    return "backpacker";
   }
 }
--- ios/backpackerTests/backpackerTests.m
+++ ios/backpackerTests/backpackerTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface RNBoilerPlateTests : XCTestCase
+@interface backpackerTests : XCTestCase
 
 @end
 
-@implementation RNBoilerPlateTests
+@implementation backpackerTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL(^)(UIView *view))test
 {
