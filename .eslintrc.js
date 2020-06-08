72070bb6986f03155135898b5a7c011ae124f5c3
Shan Wijenayaka <shanwijenayaka@outlook.com>
2020-06-08 16:49:38 +0530

    prettier further

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,10 +1,10 @@
 module.exports = {
-  root: true,
-  extends: ['@react-native-community', 'prettier'],
-  plugins: ['prettier'],
-  rules: {
-    'prettier/prettier': ['error'],
-    'no-unused-vars': 'off',
-    'react-native/no-inline-styles': 'off',
-  },
+    root: true,
+    extends: ['@react-native-community', 'prettier'],
+    plugins: ['prettier'],
+    rules: {
+        'prettier/prettier': ['error'],
+        'no-unused-vars': 'off',
+        'react-native/no-inline-styles': 'off',
+    },
 };
